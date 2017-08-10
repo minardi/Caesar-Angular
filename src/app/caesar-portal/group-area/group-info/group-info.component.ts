ff62f7dc62741155a22f17e4f2d6ac8116c50ca0
Tatiana <tetty2005@mail.ru>
2017-08-10 17:23:24 +0300

    scss changing

--- src/app/caesar-portal/group-area/group-info/group-info.component.ts
+++ src/app/caesar-portal/group-area/group-info/group-info.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'group-info',
   templateUrl: './group-info.component.html',
-  styleUrls: ['./group-info.component.css']
+  styleUrls: ['./group-info.component.scss']
 })
 export class GroupInfoComponent implements OnInit {
 
