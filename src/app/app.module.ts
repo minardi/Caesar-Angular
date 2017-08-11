c12f7f8ce3bd59ced513e69ae994bc57cf2e3364
Tatiana <tetty2005@mail.ru>
2017-08-11 12:32:57 +0300

    modules problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule }      from '@angular/core';
 
-import { AppComponent }          from './app.component';
-import { CaesarPortalComponent } from './caesar-portal/caesar-portal.component';
-import { GroupAreaComponent } from './caesar-portal/group-area/group-area.component';
-import { GroupInfoComponent } from './caesar-portal/group-area/group-info/group-info.component';
-import { GroupStudentsComponent } from './caesar-portal/group-area/group-students/group-students.component';
-import { GroupScheduleComponent } from './caesar-portal/group-area/group-schedule/group-schedule.component';
-import { GroupNotificationsComponent } from './caesar-portal/group-area/group-notifications/group-notifications.component';
+import { AppComponent }       from './app.component';
+import { CaesarPortalModule } from './caesar-portal/caesar-portal.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CaesarPortalComponent,
-    GroupAreaComponent,
-    GroupInfoComponent,
-    GroupStudentsComponent,
-    GroupScheduleComponent,
-    GroupNotificationsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    CaesarPortalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
