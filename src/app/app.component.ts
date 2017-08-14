e83d16c179d5f7826a10fd07518778264134988c
Olga04051995 <olga_lokota@ukr.net>
2017-08-14 22:32:10 +0300

    fix comments according to code and css formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Http, Headers, RequestOptions} from '@angular/http';
 
 @Component({
@@ -6,22 +6,24 @@ import {Http, Headers, RequestOptions} from '@angular/http';
   templateUrl: './app.component.html',
 })
 
-export class AppComponent {
-  constructor(private http: Http) {
-    //TODO: remove when auth logic is done 
-    const body = JSON.stringify({
-      username: "OlegShvets",
-      password: "ghd22df"
-    });
+export class AppComponent implements OnInit {
+  constructor(private http: Http) {}
+  //TODO: remove when auth logic is done
+  ngOnInit() {
+      const body = JSON.stringify({
+        username: 'OlegShvets',
+        password: 'ghd22df'
+      });
 
-    let headers = new Headers({
-      'Content-Type': 'application/json'
-    });
-    this.http.post('/login', body, {
-      headers: headers
-    }).subscribe();
-
+      let headers = new Headers({
+        'Content-Type': 'application/json'
+      });
+      this.http.post('/login', body, {
+        headers: headers
+      }).subscribe();
   }
 }
+
+
 
 
--- src/app/caesar-portal/common/models/group.ts
+++ src/app/caesar-portal/common/models/group.ts
@@ -3,5 +3,5 @@ export class Group {
   name: string;
   startDate: string;
   finishDate: string;
-  experts : string[];
+  experts: string[];
 }
