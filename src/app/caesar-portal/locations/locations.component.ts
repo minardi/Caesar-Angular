33d7e3ebd09e8344beee6a16c5fe2630dd3b0e38
Vadym <khytrykv@gmali.com>
2017-08-23 13:57:21 +0300

    added fuctional to Locations

--- src/app/caesar-portal/locations/locations.component.ts
+++ src/app/caesar-portal/locations/locations.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
-import { Response} from '@angular/http';
+import { Response } from '@angular/http';
 
 import { Location } from '../common/models/location';
 import { LocationsService } from './locations.service';
@@ -12,17 +12,16 @@ import { LocationsService } from './locations.service';
     styleUrls: ['./locations.component.scss']
 })
 export class LocationsComponent implements OnInit {
+    selectedLocatins: string[] = [];
     locations: Location[];
-    error: any;
     modalRef: BsModalRef;
+    error: any;
 
-    constructor(private modalService: BsModalService, private locationsService: LocationsService) {}
+    constructor (
+        private modalService: BsModalService,
+        private locationsService: LocationsService) {}
 
-    openModal(template: TemplateRef<any>) {
-        this.modalRef = this.modalService.show(template);
-    }
-
-    ngOnInit() {
+    ngOnInit () {
         this.locationsService.getLocations()
               .subscribe(
                   (data: Response) => this.locations = data.json(),
@@ -33,4 +32,37 @@ export class LocationsComponent implements OnInit {
               );
     }
 
+    private confirmLocations (): void {
+        const result = this.selectedLocatins.join('+');
+        this.closeModal();
+        alert(`${result} will be confirmed`);
+        // this.router.navigate([`locations/${result}`]);
+    }
+
+    private openModal (template: TemplateRef<any>): void {
+        this.modalRef = this.modalService.show(template, {class: 'delete-dialog'});
+    }
+
+    private closeModal (): void {
+        this.modalRef.hide();
+        this.selectedLocatins = [];
+    }
+
+    private addLocation ($event, location): void {
+        const index = this.selectedLocatins.indexOf(location);
+
+        if (index === -1) {
+            this.selectedLocatins.push(location);
+            $event.target.classList.toggle('selected');
+        } else {
+            this.selectedLocatins.splice(index, 1);
+            $event.target.classList.toggle('selected');
+        }
+    }
+
+    private addAndConfirmLocation (location) {
+        this.selectedLocatins = [];
+        this.selectedLocatins.push(location);
+        this.confirmLocations();
+    }
 }
