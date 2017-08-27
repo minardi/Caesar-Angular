143748b8163a55561c1b0d857d49e3f85f97e203
Tatiana <tetty2005@mail.ru>
2017-08-27 23:27:07 +0300

    group info

--- src/app/caesar-portal/group-area/group-area.component.ts
+++ src/app/caesar-portal/group-area/group-area.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { GroupService } from "../common/services/group.service";
-import { Response } from '@angular/http';
 import { Router, NavigationEnd } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 import { Group } from '../common/models/group';
+import { Location } from '../common/models/location';
+import { ErrorHandlingService } from '../common/services/error-handling.service';
 
 @Component({
     selector: 'group-area',
@@ -14,12 +15,16 @@ import { Group } from '../common/models/group';
 
 export class GroupAreaComponent implements OnInit, OnDestroy {
     activeTab;
-    group: Group = new Group();
     paramsSubscription: Subscription;
+    group: Group = new Group();
+    location: Location = new Location();
+    stage: string[];
+
 
     constructor (private router: Router, 
                  private route: ActivatedRoute, 
-                 private groupService: GroupService) {
+                 private groupService: GroupService,
+                 private errorHandlingService: ErrorHandlingService) {
         router.events.subscribe((e) => {
             if (e instanceof NavigationEnd) {
                 this.activeTab = e.url.substr(e.url.lastIndexOf('/'));
@@ -37,12 +42,36 @@ export class GroupAreaComponent implements OnInit, OnDestroy {
     }
 
     private getGroup (groupId: number) {
-        this.groupService.get(groupId).then(
+        this.groupService.get(groupId).subscribe(
             (group: Group) => {
                 this.group = group;
-                this.groupService.setGroup(group);
+                this.changeIdCurrent(groupId);
+                this.getLocation(this.group.links['location'].href);
+                this.getStage(this.group.links['status'].href);
             },
-            error => console.log(error)
+            error => this.errorHandlingService.check(error.status)
+        );
+    }
+
+    changeIdCurrent(id: number): void {
+        this.groupService.changeIdCurrent(id);
+    }
+
+    private getLocation (locationUrl: string) {
+        this.groupService.getLocation(locationUrl).subscribe(
+            (location: Location) => {
+                this.location = location;
+            },
+            error => this.errorHandlingService.check(error.status)
+        );
+    }
+
+    private getStage (stageUrl: string) {
+        this.groupService.getParametr(stageUrl).subscribe(
+            (stage: string[]) => {
+                this.stage = stage;
+            },
+            error => this.errorHandlingService.check(error.status)
         );
     }
 
